a0ee7ece67a251db14e098482b19041f10021d8f
martinnicolas <ttincho77@gmail.com>
2018-07-29 02:43:00 -0300

    Agrego seed para personas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Persona.create(dni: 31343209, apellido: 'Torsiglieri', nombre: 'Marcos')
+Persona.create(dni: 32343209, apellido: 'Palama', nombre: 'Omar Arnaldo')
+Persona.create(dni: 33343209, apellido: 'Gonzalez', nombre: 'Cristian')
+Persona.create(dni: 34343209, apellido: 'Ruben', nombre: 'Marco')
+Persona.create(dni: 35343209, apellido: 'Herrera', nombre: 'German')
+Persona.create(dni: 36343209, apellido: 'Carrizo', nombre: 'Federico')
