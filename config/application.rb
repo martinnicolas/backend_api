f25aa16b24ab2a2c7d1e32999b547aed10f21701
martinnicolas <ttincho77@gmail.com>
2018-12-28 17:20:56 -0300

    Dejo configuracion CORS a como deberia haber estado en un principio

--- config/application.rb
+++ config/application.rb
@@ -27,11 +27,5 @@ module BackendApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :delete]
-      end
-    end
   end
 end
